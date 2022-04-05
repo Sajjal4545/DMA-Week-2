f7a0d4d18328c88d5afe4d01f690cdf4bdf5219d
SajjalKoirala <ksajjal18@tbc.edu.np>
2022-04-05 22:12:40 +0545

    Readme files added to helloconstraint

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-<img src="gif/LayoutChallenge.gif" width="400" />
-<img src="gif/LayoutChallengeland.gif" width="400" />
+<img src="gif/HelloConstraintland.gif" width="400" />
+<img src="gif/HelloConstraintPixel.gif" width="400" />
+<img src="gif/HelloConstraintTablet.gif" width="400" />
+<img src="gif/HelloConstrainttabletland.gif" width="400" />
--- app/src/main/java/com/example/hellotoastlab1/MainActivity.java
+++ app/src/main/java/com/example/hellotoastlab1/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.hellotoastlab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,24 +13,47 @@ public class MainActivity extends AppCompatActivity {
 
     private int mCount=0;
     private TextView mShowCount;
+    //private Button zero_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        //countZero(mShowCount.setBackgroundColor());
+
     }
 
     public void showToast(View view) {
-        Toast toast = Toast.makeText(this, R.string.toast_message,
-                Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
         toast.show();
-
     }
 
     public void countUp(View view) {
         mCount++;
-        if (mShowCount != null)
+        //countZero(view.setBackgroundColor(Color.DKGRAY));
+
+        if (mShowCount != null) {
             mShowCount.setText(Integer.toString(mCount));
+
+        }
+        if(mCount%2==0){
+            view.setBackgroundColor(Color.GREEN);
+        }
+        else if(mCount%2!=0){
+            view.setBackgroundColor(Color.CYAN);
+        }
+        view = findViewById(R.id.zero);
+        view.setBackgroundColor(Color.MAGENTA);
+
+    }
+
+    public void countZero(View view) {
+        mCount = 0;
+        mShowCount.setText(Integer.toString(mCount));
+        view.setBackgroundColor(Color.DKGRAY);
+        view = findViewById(R.id.button_count);
+        view.setBackgroundColor(Color.GREEN);
+        //countUp(findViewById(R.id.button_count).setBackgroundColor(Color.RED));
     }
 }
